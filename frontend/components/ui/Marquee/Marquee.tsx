9b3b49e41ed69d112f41200edc33f62412a57511
João Otávio <joaotavioos@outlook.com>
2021-11-22 21:36:47 -0300

    Add Marquee and Hero component

---
+++ frontend/components/ui/Marquee/Marquee.tsx
@@ -0,0 +1,23 @@
+import { FC, ReactNode } from 'react'
+import style from './Marquee.module.css'
+import Ticker from 'react-ticker'
+import classnames from 'classnames'
+
+interface Props {
+  children: ReactNode[]
+  variant?: 'primary' | 'secondary'
+}
+
+const Marquee: FC<Props> = ({ children, variant = 'primary' }) => {
+  const rootClassName = classnames(style.root, {
+    [style.secondary]: variant === 'secondary'
+  })
+
+  return (
+    <div className={rootClassName}>
+      <Ticker>{() => <div className={style.container}>{children}</div>}</Ticker>
+    </div>
+  )
+}
+
+export default Marquee
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,7 +3,7 @@ import getAllProducts from '@framework/product/get-all-products'
 import { getConfig } from 'framework/shopify/api/config'
 import { Layout } from '@components/common'
 import { ProductCard } from '@components/product'
-import { Grid } from '@components/ui'
+import { Grid, Hero, Marquee } from '@components/ui'
 
 export async function getStaticProps() {
   const config = getConfig()
@@ -21,11 +21,30 @@ export async function getStaticProps() {
 const Home = ({ products }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <>
+      <Grid>
+        {products.slice(0, 3).map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </Grid>
+      <Hero
+        headline="Cookies, ice cream and muffin"
+        description="Marshmallow tart jelly icing cotton candy tootsie roll cotton candy candy canes. Cake liquorice sesame snaps. Cupcake cake cheesecake pie marshmallow lollipop soufflé marshmallow dessert. Cheesecake jujubes halvah chupa chups lollipop tootsie roll. Jelly-o tiramisu jelly toffee cake croissant lemon drops pudding. Donut sesame snaps gummi bears toffee. Sesame snaps jelly-o oat cake chocolate marzipan cake lollipop. Gingerbread cheesecake jujubes fruitcake cake. Tiramisu cotton candy marzipan candy canes oat cake pudding bonbon."
+      />
+      <Marquee>
+        {products.slice(0, 3).map((product) => (
+          <ProductCard variant="slim" key={product.id} product={product} />
+        ))}
+      </Marquee>
       <Grid layout="B">
         {products.slice(0, 3).map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </Grid>
+      <Marquee variant="secondary">
+        {products.slice(0, 3).map((product) => (
+          <ProductCard variant="slim" key={product.id} product={product} />
+        ))}
+      </Marquee>
     </>
   )
 }
